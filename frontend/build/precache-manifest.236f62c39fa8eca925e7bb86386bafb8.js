da99179709f1d3ab53cef3dd429eec2f94957cfa
Jack H <8584913+jckhxn@users.noreply.github.com>
2020-10-19 15:40:57 -0400

    gitignore stuff

---
+++ frontend/build/precache-manifest.236f62c39fa8eca925e7bb86386bafb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4593285889104b310ddb53f669b6e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f2286c8a4c2439aaa8",
+    "url": "/static/css/main.514eff18.chunk.css"
+  },
+  {
+    "revision": "9e50589c2d3e95c9de51",
+    "url": "/static/js/2.cb7134ce.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.cb7134ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f2286c8a4c2439aaa8",
+    "url": "/static/js/main.b5d5c536.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "a9a60d2a07dec6ca859f26a838c9cff3",
+    "url": "/static/media/header.a9a60d2a.jpg"
+  }
+]);
