5b6e78e88c873538c7e4264e257b9e863f0e9f3f
Jack H <8584913+jckhxn@users.noreply.github.com>
2020-11-04 20:39:45 -0500

    :sparkles:

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -76,8 +76,8 @@ router.post("/create-checkout-session", async (req, res) => {
     },
     customer_email:email,
     // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-    success_url: `${domainURL}/success`,
-    cancel_url: `${domainURL}/cart`,
+    success_url: `${domainURL}success`,
+    cancel_url: `${domainURL}cart`,
     });
     
 
