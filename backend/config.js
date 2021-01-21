f2afb802372290167b1ffad09ff8838afd07dfdd
Jack H <8584913+jckhxn@users.noreply.github.com>
2021-01-20 21:17:23 -0500

    Oopsie

--- backend/config.js
+++ backend/config.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://jack:112358@storefront.g0cel.mongodb.net/storefront?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL ||
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AXj_EpQRv_1Vl5Uqrw_Aofe_OHXwAhU43qg2h3oVZWh8s1bQmftYu_V5-VQOnkQ63CGDh173zsEC9D5V',
-  accessKeyId: process.env.accessKeyId || 'AKIAJKJFZYJJROJ75TAQ',
-  secretAccessKey: process.env.secretAccessKey || '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',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 
+  accessKeyId: process.env.accessKeyId || 
+  secretAccessKey: process.env.secretAccessKey || 
 };
