a091a718026bef4e040b172a2aaf041f3d425c8f
Jack H <8584913+jckhxn@users.noreply.github.com>
2021-01-21 09:48:13 -0500

    Added keys for production

--- backend/config.js
+++ backend/config.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mon',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://jack:112358@rrprods.kmzis.mongodb.net/rrpods?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AX
-  accessKeyId: process.env.accessKeyId || ',
-  secretAccessKey: process.env.secretAccessKey || '
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AXj_EpQRv_1Vl5Uqrw_Aofe_OHXwAhU43qg2h3oVZWh8s1bQmftYu_V5-VQOnkQ63CGDh173zsEC9D5V',
+  accessKeyId: process.env.accessKeyId || 'AKIAIWGY4P4THKFZULMQ',
+  secretAccessKey: process.env.secretAccessKey || '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',
 };
