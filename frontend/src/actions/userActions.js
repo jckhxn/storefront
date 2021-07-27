386b6c9bc876a59b64ee068d6130309f5359ddf4
Jack H <8584913+jckhxn@users.noreply.github.com>
2021-07-27 10:44:06 -0400

    updated dependds

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,70 +1,98 @@
 import Axios from "axios";
-import Cookie from 'js-cookie';
+import Cookie from "js-cookie";
 import {
-  USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS,
-  USER_SIGNIN_FAIL, USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_LOGOUT, USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS, USER_UPDATE_FAIL
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNIN_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_LOGOUT,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL,
 } from "../constants/userConstants";
 
-const update = ({ userId, name, email, password, coupon }) => async (dispatch, getState) => {
-  const { userSignin: { userInfo } } = getState();
-  dispatch({ type: USER_UPDATE_REQUEST, payload: { userId, name, email, password ,coupon} });
+const update = ({ userId, name, email, password, coupon }) => async (
+  dispatch,
+  getState
+) => {
+  const {
+    userSignin: { userInfo },
+  } = getState();
+  dispatch({
+    type: USER_UPDATE_REQUEST,
+    payload: { userId, name, email, password, coupon },
+  });
   try {
-    const { data } = await Axios.put("/api/users/" + userId,
-      { name, email, password, coupon}, {
-      headers: {
-        Authorization: 'Bearer ' + userInfo.token
+    const { data } = await Axios.put(
+      "/api/users/" + userId,
+      { name, email, password, coupon },
+      {
+        headers: {
+          Authorization: "Bearer " + userInfo.token,
+        },
       }
-    });
+    );
     dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
-    Cookie.set('userInfo', JSON.stringify(data));
+    Cookie.set("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({ type: USER_UPDATE_FAIL, payload: error.message });
   }
-}
-const setUserCoupon  = ({ userId, coupon }) => async (dispatch, getState) => {
-  const { userSignin: { userInfo } } = getState();
-  dispatch({ type: USER_UPDATE_REQUEST, payload: { userId, coupon} });
+};
+
+const setUserCoupon = ({ userId, coupon }) => async (dispatch, getState) => {
+  const {
+    userSignin: { userInfo },
+  } = getState();
+  dispatch({ type: USER_UPDATE_REQUEST, payload: { userId, coupon } });
   try {
-    const { data } = await Axios.put("/api/users/" + userId,
-      {  coupon}, {
-      headers: {
-        Authorization: 'Bearer ' + userInfo.token
+    const { data } = await Axios.put(
+      "/api/users/" + userId,
+      { coupon },
+      {
+        headers: {
+          Authorization: "Bearer " + userInfo.token,
+        },
       }
-    });
+    );
     dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
-    Cookie.set('userInfo', JSON.stringify(data));
+    Cookie.set("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({ type: USER_UPDATE_FAIL, payload: error.message });
   }
-}
+};
 
 const signin = (email, password) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
   try {
     const { data } = await Axios.post("/api/users/signin", { email, password });
-  
+
     dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
-    Cookie.set('userInfo', JSON.stringify(data));
+    Cookie.set("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
   }
-}
+};
 
 const register = (name, email, password) => async (dispatch) => {
   dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
   try {
-    const { data } = await Axios.post("/api/users/register", { name, email, password });
+    const { data } = await Axios.post("/api/users/register", {
+      name,
+      email,
+      password,
+    });
     dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-    Cookie.set('userInfo', JSON.stringify(data));
+    Cookie.set("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
   }
-}
+};
 
 const logout = () => (dispatch) => {
   Cookie.remove("userInfo");
-  dispatch({ type: USER_LOGOUT })
-}
+  dispatch({ type: USER_LOGOUT });
+};
 
 export { signin, register, logout, update, setUserCoupon };
